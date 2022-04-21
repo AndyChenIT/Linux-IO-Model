c18ae057d02b24495a3f591fdc1190914b265cbb
AndyChen <1191656653@qq.com>
2022-04-21 08:53:28 +0800

    1

--- io-poll/poll.c
+++ io-poll/poll.c
@@ -8,11 +8,11 @@
 
 int main()
 {
-    // 新建并初始化文件描述符集
+    // 1) 新建并初始化文件描述符集
     struct pollfd fds[MAX_NUM_FDS];
     int nfds = 0;
 
-    // tcp socket配置和监听
+    // 2) tcp socket配置和监听
     int sock_listen = socket(AF_INET, SOCK_STREAM, 0);
     if(sock_listen < 0) {
         perror("failed to create sock_listen");
@@ -29,6 +29,7 @@ int main()
     }
     printf("initialize addr_listen success, ip:port=%s:%d\n", IPADDR, PORT);
 
+    // 3) 绑定监听
     if(bind(sock_listen, (struct sockaddr *)&addr_listen, sizeof(addr_listen)) < 0) {
         perror("bind socket & addr failed");
         return 1;
@@ -41,7 +42,7 @@ int main()
     }
     printf("listen on ip:port success, ip:port= %s:%d\n", IPADDR, PORT);
     
-    // 对socket描述符上关心的事件进行注册
+    // 4) 初始化准备工作，将第一个数组下标监听套接字读事件
     fds[0].fd = sock_listen;
     fds[0].events = POLLIN;
     ++ nfds;
@@ -50,10 +51,10 @@ int main()
     
         int i;
     
-        // 调用poll阻塞等待，等待时间为永远等待直到事件发生
+        // 5) 调用poll阻塞等待，等待时间为永远等待直到事件发生
         poll(fds, nfds, -1);
     
-        // 测试是否有客户端发起连接请求，如果有则接受并把新建的描述符加入监控
+        // 6) 测试是否有客户端发起连接请求，如果有则接受并把新建的描述符加入监控
         if(fds[0].revents & POLLIN) {
             int sock_conn = accept(sock_listen, NULL, NULL); 
     
@@ -63,7 +64,7 @@ int main()
             ++ nfds;
         }
         
-        // 对其他描述符发生的事件进行适当处理
+        // 7) 对其他描述符发生的事件进行适当处理
         for(i=1; i<nfds+1; ++i) {
             if( (fds[i].revents & POLLIN) && (fds[i].revents & POLLOUT) ) {
                 doEchoActionInPoll(fds, MAX_NUM_FDS, i);
